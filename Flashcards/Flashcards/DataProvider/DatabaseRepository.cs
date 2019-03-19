4e8570b4d74654a65d900f588791d3f4f8ce5488
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-03-19 15:16:47 +0100

    Repository moved

---
+++ Flashcards/Flashcards/DataProvider/DatabaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Flashcards.Models
+{
+    public class DatabaseRepository
+    {
+        SQLiteConnection database;
+        static object locker = new object();
+        public DatabaseRepository()
+        {
+            database = DependencyService.Get<ISQLite>().GetConnection();
+            database.CreateTable<Phrase>();
+        }
+        public IEnumerable<Phrase> GetPhrases()
+        {
+            lock (locker)
+            {
+                return (from p in database.Table<Phrase>()
+                        select p).ToList();
+            }
+        }
+        public List<string> GetGroups()
+        {
+            lock (locker)
+            {
+                List<string> groups = new List<string>();
+                var context = database.Table<Phrase>();
+                foreach(Phrase phrase in context)
+                {
+                    groups.Add(phrase.Group);
+                }
+                return groups.ToList();
+            }
+        }
+        public int SavePhrase(Phrase phrase)
+        {
+            lock (locker)
+            {
+                if (phrase.Id != 0)
+                {
+                    database.Update(phrase);
+                    return phrase.Id;
+                }
+                else
+                {
+                    return database.Insert(phrase);
+                }
+            }
+        }
+    }
+}
