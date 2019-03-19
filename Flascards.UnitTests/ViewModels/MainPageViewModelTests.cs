7bc682ee87220514c37c06e6f0ec1f9a589ba2bc
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-03-19 19:33:51 +0100

    Added 2 tests;

--- Flascards.UnitTests/ViewModels/MainPageViewModelTests.cs
+++ Flascards.UnitTests/ViewModels/MainPageViewModelTests.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using Flashcards.DataProvider;
+using Flashcards.ViewModels;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Flascards.UnitTests.ViewModels
 {
     public class MainPageViewModelTests
     {
+        private MainPageViewModel _viewModel;
+
+        public MainPageViewModelTests()
+        {
+            var mainDataProviderMock = new Mock<IMainDataProvider>();
+            mainDataProviderMock.Setup(dp => dp.GetGroups())
+              .Returns(new List<string>
+              {
+          "Group #1",
+          "Group #2",
+          "Group #3"
+             });
+            _viewModel = new MainPageViewModel(mainDataProviderMock.Object);
+        }
+        [Fact]
+        public void ShouldLoadGroupsOnlyOnce()
+        {
+            _viewModel.LoadGroups();
+            _viewModel.LoadGroups();
+
+            Assert.Equal(3, _viewModel.Groups.Count);
+        }
+        [Fact]
+        public void ShouldLoadGroups()
+        {
+            _viewModel.LoadGroups();
+            Assert.Equal(3, _viewModel.Groups.Count);
+            var friend = _viewModel.Groups[0];
+            Assert.NotNull(friend);
+            Assert.Equal("Group #1", friend);
+        }
     }
 }
