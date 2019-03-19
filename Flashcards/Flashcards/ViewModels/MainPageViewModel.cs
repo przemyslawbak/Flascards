89b3c6b8f32a3219f83bbcff01a7a3a547987ad2
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-03-19 20:30:15 +0100

    Added NewPhraseEdit prop; Added NewPhraseCommand; Added not implemented method OnNewPhraseExecute

--- Flashcards/Flashcards/ViewModels/MainPageViewModel.cs
+++ Flashcards/Flashcards/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Flashcards.DataProvider;
+﻿using Flashcards.Command;
+using Flashcards.DataProvider;
 using Flashcards.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace Flashcards.ViewModels
 {
@@ -14,11 +16,23 @@ namespace Flashcards.ViewModels
     {
         private IMainDataProvider _dataProvider;
         public List<string> Groups { get; set; }
+        public bool NewPhraseEdit { get; set; }
         public MainPageViewModel(IMainDataProvider dataProvider) //ctor
         {
             Groups = new List<string>();
             _dataProvider = dataProvider;
+
+            //commands
+            NewPhraseCommand = new DelegateCommand(OnNewPhraseExecute);
         }
+
+        public ICommand NewPhraseCommand { get; private set; }
+
+        private void OnNewPhraseExecute(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
         public void LoadGroups() //loads group list from the DB
         {
             Groups.Clear();
