04d56dcade98952bd6bf291d53ca505e8d073860
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-03-19 19:35:18 +0100

    Created data provider interface for the MainPage

---
+++ Flashcards/Flashcards/DataProvider/IMainDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flashcards.DataProvider
+{
+    public interface IMainDataProvider
+    {
+        List<string> GetGroups();
+    }
+}
---
+++ Flashcards/Flashcards/DataProvider/MainDataProvider.cs
@@ -0,0 +1,23 @@
+﻿using Flashcards.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flashcards.DataProvider
+{
+    public class MainDataProvider : IMainDataProvider
+    {
+        private Func<IDatabaseRepository> _dataServiceCreator;
+        public MainDataProvider(Func<IDatabaseRepository> dataServiceCreator)
+        {
+            _dataServiceCreator = dataServiceCreator;
+        }
+        public List<string> GetGroups()
+        {
+            using (var dataService = _dataServiceCreator())
+            {
+                return dataService.GetGroups();
+            }
+        }
+    }
+}
