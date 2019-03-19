e0488d1d846154119e0d799147e2c2d33f7a8d47
przemyslawbak <przemyslaw.bak@simple-mail.net>
2019-03-19 19:33:22 +0100

    Registered IMainDataProvider;

--- Flashcards/Flashcards/Startup/BootStrapper.cs
+++ Flashcards/Flashcards/Startup/BootStrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Flashcards.DataAccess;
 using Flashcards.DataProvider;
 using Flashcards.ViewModels;
 using System;
@@ -18,6 +19,8 @@ namespace Flashcards.Startup
                 .AsSelf();
             builder.RegisterType<MainPageViewModel>()
                 .AsSelf();
+            builder.RegisterType<MainDataProvider>()
+                .As<IMainDataProvider>();
             return builder.Build();
         }
     }
